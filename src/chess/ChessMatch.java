2c8769c72a6c94e0911f34781a3db60d43ec82f4
Rui Pereira <rui6.319@hotmail.com>
2024-07-15 17:40:43 +0100

    Checkmate logic

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -17,6 +17,7 @@ public class ChessMatch {
 	private Color currentPlayer;
 	private Board board;
 	private boolean check;
+	private boolean checkMate;
 	
 	private List<Piece> piecesOnTheBoard = new ArrayList<>();
 	private List<Piece> capturedPieces = new ArrayList<>();
@@ -44,6 +45,13 @@ public class ChessMatch {
 	{
 		return check;
 	}
+	
+	
+	public boolean getCheckMate()
+	{
+		return checkMate;
+	}
+	
 	
 	
 	public ChessPiece[][] getPieces()
@@ -83,7 +91,15 @@ public class ChessMatch {
 		
 		check = (testCheck(opponent(currentPlayer)))? true : false;
 		
+		if(testCheckMate(opponent(currentPlayer)))
+		{
+		  checkMate = true;
+		}
+		else {
 		nextTurn();
+		}
+		
+		
 		return (ChessPiece)capturedPiece;
 		
 	}
@@ -180,6 +196,37 @@ public class ChessMatch {
 	    }
 	    return false;
 	}
+	private boolean testCheckMate(Color color)
+	{
+		if(!testCheck(color))
+		{
+			return false;
+		}
+		List<Piece> list = piecesOnTheBoard.stream().filter(x -> ((ChessPiece)x).getColor() == color).collect(Collectors.toList());
+		for (Piece p: list)
+		{
+			boolean[][] mat = p.possibleMoves();
+			for(int i = 0; i<board.getRows(); i++)
+			{
+				for(int j = 0 ; j<board.getColumns(); j++)
+				{
+				  if(mat[i][j])
+				  {
+					position source = ((ChessPiece)p).getChessPosition().toPosition();
+					position target = new position(i, j);
+					Piece capturedPiece = makeMove(source, target);
+					boolean testCheck = testCheck(color);
+					undoMove(source, target,capturedPiece);
+					if(!testCheck)
+					{
+						return false;
+					}
+				}
+			}
+		}
+	}
+	return true;
+}
 	
 	
 	
@@ -192,20 +239,24 @@ public class ChessMatch {
 	 
 	private void initialSetup()
 	{
-		//placeNewPiece('b', 6, new Rook(board, Color.WHITE));
-		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		/*placeNewPiece('e', 8, new King(board, Color.BLACK));
 		placeNewPiece('e', 1, new King(board, Color.WHITE));
 		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
         placeNewPiece('c', 2, new Rook(board, Color.WHITE));
         placeNewPiece('d', 2, new Rook(board, Color.WHITE));
         placeNewPiece('e', 2, new Rook(board, Color.WHITE));
         placeNewPiece('d', 1, new King(board, Color.WHITE));
-
         placeNewPiece('c', 7, new Rook(board, Color.BLACK));
         placeNewPiece('c', 8, new Rook(board, Color.BLACK));
         placeNewPiece('d', 7, new Rook(board, Color.BLACK));
         placeNewPiece('e', 7, new Rook(board, Color.BLACK));
-        placeNewPiece('d', 8, new King(board, Color.BLACK));
+        placeNewPiece('d', 8, new King(board, Color.BLACK));*/
+		placeNewPiece('h',7,new Rook(board, Color.WHITE));
+		placeNewPiece('d',1,new Rook(board, Color.WHITE));
+		placeNewPiece('e',1,new King(board, Color.WHITE));
+		placeNewPiece('b',8,new Rook(board, Color.BLACK));
+		placeNewPiece('a',8,new King(board, Color.BLACK));
+		
 	}
 
 }
